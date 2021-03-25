ac62c9ff28d33dacfb373047af9732d88986779d
Adrián Sánchez Nievares <66979377+adriansanchez88@users.noreply.github.com>
2021-03-25 22:35:54 +0100

    apiKeys config updated

--- src/config/index.js
+++ src/config/index.js
@@ -20,10 +20,10 @@ module.exports = {
     },
     mapbox: {
         pathBase: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
-        apikey: process.env.MAPBOX_API_KEY
+        apikey: process.env.MAPBOX_API_KEY || 'pk.eyJ1IjoiYWRyaWFuc2FuY2hlejg4IiwiYSI6ImNrbWtvY2QwMTEzMGIyb2s1NGdsbThrMmEifQ._e2ASWwDgTP33eJaqGDSUQ'
     },
     openweathermap: {
         pathBase: 'https://api.openweathermap.org/data/2.5/weather',
-        apikey: process.env.OPENWEATHERMAP_API_KEY
+        apikey: process.env.OPENWEATHERMAP_API_KEY || 'df9f954306a200048efe0cd4c8f336b9'
     }
 }
